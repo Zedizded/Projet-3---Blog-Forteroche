03b62ae473f00e23f08ee32fcabbadca80da689b
Zedizded <zedmatt@live.fr>
2017-06-01 15:59:12 +0200

    Itération 7 : Modification de la BDD et du design

---
+++ db/database.sql
@@ -0,0 +1,4 @@
+create database if not exists p3_blog_jforteroche character set utf8 collate utf8_unicode_ci;
+use p3_blog_jforteroche;
+
+grant all privileges on p3_blog_jforteroche.* to 'jforteroche_admin'@'localhost' identified by 'fHt4lvhk2SVwEkAB';
